da8f2aac548277d298973f420fd509e86c348b2d
Primus <18100664+optimus9811@users.noreply.github.com>
2022-12-11 18:59:50 +0530

    Cleanup

--- product.mk
+++ product.mk
@@ -2,19 +2,3 @@
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.adb.shell=/system_ext/bin/bash
 
-# Google Apps
-ifeq ($(WITH_GMS), true)
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-endif
-
-# ih8sn
-PRODUCT_PACKAGES += ih8sn
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ih8sn/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
-
-# iperf3
-PRODUCT_PACKAGES += iperf3
-
-# tinymix
-PRODUCT_PACKAGES += tinymix
